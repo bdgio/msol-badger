f42c4b8a106712d9ba06361d2879195606936cab
kayaelle <kerri@achievery.com>
2015-06-08 15:39:22 -0400

    Adding contact form and contact details to org page

--- routes/index.js
+++ routes/index.js
@@ -162,6 +162,8 @@ exports.define = function defineRoutes(app) {
   app.get('/explore', issuer.findAll,render.explore);
   app.get('/about', render.about);
   // app.get('/faq', render.faq);
+  app.get('/contact-us', render.contactUs);
+  app.post('/contact-us', user.contactUs);
   
   app.get('/mybadges/:editFunction',
   [badge.findByUser
